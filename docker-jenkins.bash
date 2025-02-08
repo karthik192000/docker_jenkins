2759a1f5a5ead0cb36d83aed307efc63df7ae637
Karthik Bankapur <bankapurkarthik@gmail.com>
2025-02-08 15:40:01 +0530

    Docker bash

---
+++ docker-jenkins.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -p 8080:8080 -p 50000:50000 --name jenkins -e DOCKER_TLS_VERIFY=1 -e DOCKER_HOST=tcp://docker:2376 -e DOCKER_CERT_PATH=/certs/client -v /mnt/jenkins-data:/var/jenkins_home -v /mnt/jenkins-docker-certs:/certs/client:ro --privileged jenkins/jenkins:jdk17
