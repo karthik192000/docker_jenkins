717061358041939377ff2c0b1681c9a4b106dbb2
Karthik Bankapur <bankapurkarthik@gmail.com>
2025-02-09 11:11:14 +0530

    DOcker

--- docker-agent.bash
+++ docker-agent.bash
@@ -1,2 +1,13 @@
 #!/bin/bash
-docker run -d --restart=always --name docker-socket-proxy -v /var/run/docker.sock:/var/run/docker.sock -p 2375:2375 --env CONTAINERS=1 --env POST=1 --env SYSTEM=1 --env IMAGES=1 --env BUILD=1 tecnativa/docker-socket-proxy
+docker run -d --restart=always --name docker-socket-proxy \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -p 2375:2375 \
+  --env CONTAINERS=1 \
+  --env POST=1 \
+  --env SYSTEM=1 \
+  --env IMAGES=1 \
+  --env BUILD=1 \
+  --env NETWORKS=1 \  # Add this
+  --env EXEC=1 \      # Add this
+  --env TASKS=1 \     # Add this
+  tecnativa/docker-socket-proxy
