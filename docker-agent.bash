0d08a862fb166c37834f43f1884e8e33c82a23cf
Karthik Bankapur <bankapurkarthik@gmail.com>
2025-02-09 10:55:55 +0530

    Docker Socket Proxy

---
+++ docker-agent.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d --restart=always --name docker-socket-proxy -v /var/run/docker.sock:/var/run/docker.sock -p 2375:2375 --env CONTAINERS=1 --env POST=1 --env SYSTEM=1 --env IMAGES=1 --env BUILD=1 tecnativa/docker-socket-proxy
