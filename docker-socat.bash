1c930ebe17acbc6cb878c611d1f57775dcd523bf
Karthik Bankapur <bankapurkarthik@gmail.com>
2025-02-09 19:58:48 +0530

    restart always socat

--- docker-socat.bash
+++ docker-socat.bash
@@ -1,1 +1,1 @@
-docker run -d --name socat -v /var/run/docker.sock:/var/run/docker.sock -p 2375:2375 alpine/socat tcp-listen:2375,fork,reuseaddr unix-connect:/var/run/docker.sock
+docker run -d --restart=always --name socat -v /var/run/docker.sock:/var/run/docker.sock -p 2375:2375 alpine/socat tcp-listen:2375,fork,reuseaddr unix-connect:/var/run/docker.sock
